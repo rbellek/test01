bbad84f1fd9ec09e2851226fd125b5d83d49b7ff
Ramazan <Ramazan@RamazanPC>
2018-02-17 17:49:33 +0200

    first

---
+++ Test01/src/Test01.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Test01.cpp
+// Author      : Ramazan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	using namespace std;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	return 0;
+}
